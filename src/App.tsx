d300a47c477146fd9a718f8dfc9560b06b49442f
KITE-hub <yka110mail@gmail.com>
2024-10-19 01:27:04 +0900

    2nd

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,130 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import GuideButton from './components/GuideButton';
+import './normalize.css';
 import './App.css';
+import HeaderImg from './images/headerimg.png';
 
 function App() {
+  const [beatValue,setBeatValue] = useState<number>(4);
+  const [countValue,setCountValue] = useState<number>();
+  const [bpmValue,setBpmValue] = useState<number>();
+  const [hitValue,setHitValue] = useState<number>();
+  const [hitLength, setHitLength] = useState<number | null>(null);
+  const [hitLengthReal, setHitLengthReal] = useState<number | null>(null);
+  const [hitPerSecond, sethitPerSecond] = useState<number | null>(null);
+  const [hitLimit, sethitLimit] = useState<number | null>(null);
+  const [hitDifference, sethitDifference] = useState<number | null>(null);
+  const handleBeat = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setBeatValue(Number(e.target.value));
+  };
+  const handleCount = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCountValue(Number(e.target.value));
+  };
+  const handleBpm = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setBpmValue(Number(e.target.value));
+  };
+  const handleHit = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setHitValue(Number(e.target.value));
+  };
+
+  useEffect(() => {
+    if (beatValue && countValue && bpmValue) {
+      const length = (240*Number(countValue)/Number(beatValue) - 5) / Number(bpmValue);
+      setHitLength(parseFloat(length.toFixed(4)));
+      setHitLengthReal(length);
+    } 
+    else {
+      setHitLength(null);
+    }
+  }, [beatValue,countValue, bpmValue]);
+
+  useEffect(() => {
+    if(hitLengthReal && hitValue){
+      const mainresult = Number(hitValue)/Number(hitLengthReal);
+      sethitPerSecond(parseFloat(mainresult.toFixed(4)));
+    }
+    else{
+      sethitPerSecond(null);
+    }
+  },[hitLength,hitValue]);
+
+  useEffect(() => {
+    if(hitLengthReal){
+      const limit = Math.ceil(Number(hitLengthReal)*60);
+      sethitLimit(limit);
+    }
+    else{
+      sethitLimit(null);
+    }
+  },[hitLengthReal]);
+
+  useEffect(() => {
+    if(hitValue && hitLimit){
+      const dif = Number(hitLimit) - Number(hitValue);
+      sethitDifference(dif);
+    }
+    else{
+      sethitDifference(null);
+    }
+  })
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <img src={HeaderImg} />
+        <h1 className="title" >連打計算サイト</h1>
+        <GuideButton/>
       </header>
+      <div className="bg">
+        <section className="input">
+          <div className="inputField">
+            <div className="inputField1">
+              <div className="inputboxspecial">
+                <label className="labelSp" htmlFor="beat">リズム単位</label>
+              <select id="beat" value={beatValue} onChange={(e) => handleBeat(e)}>
+                <option value="4">4</option>
+                <option value="8">8</option>
+                <option value="12">12</option>
+                <option value="16">16</option>
+                <option value="24">24</option>
+                <option value="48">48</option>
+              </select>
+              </div>
+              <div className="inputText">[分音符]　×</div>
+              <div className="inputbox">
+                <label htmlFor="count">個数</label>
+                <input type="number" id="count" name="" value={countValue} onChange={(e) => handleCount(e)}></input>
+              </div>
+              <button className="bm" onClick={() => setCountValue(Number(countValue)-1)}>-</button>
+              <button className="bp" onClick={() => setCountValue(Number(countValue)+1)}>+</button>
+            </div>
+            <div className="inputField2">
+              <div className="inputbox">
+                <label htmlFor="bpm">BPM</label>
+                <input type="number" id="bpm" name="" value={bpmValue} onChange={(e) => handleBpm(e)}></input>
+              </div>
+              <button className="bm" onClick={() => setBpmValue(Number(bpmValue)-1)}>-</button>
+              <button className="bp" onClick={() => setBpmValue(Number(bpmValue)+1)}>+</button>
+            </div>
+            <div className="inputField3">
+              <div className="inputbox">
+                <label htmlFor="hit">入力打数</label>
+                <input type="number" id="hit" name="" value={hitValue} onChange={(e) => handleHit(e)}></input>
+              </div>
+              <button className="bm" onClick={() => setHitValue(Number(hitValue)-1)}>-</button>
+              <button className="bp" onClick={() => setHitValue(Number(hitValue)+1)}>+</button>
+              <div className="inputText">[打]</div>
+            </div>
+          </div>
+        </section>
+        <hr/>
+        <section className="output">
+          <p>秒速:　<strong>{hitPerSecond} [打/秒]</strong></p>
+          <p className="smalltext">(理論値まであと　<strong>{hitDifference} [打]</strong>)</p>
+          <p>連打秒数:　<strong>{hitLength} [秒]</strong></p>
+          <p>理論値:　<strong>{hitLimit} [打]</strong></p>
+        </section>
+      </div>
     </div>
   );
 }
---
+++ src/components/GuideButton.tsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import {Button, Link, Dialog, DialogContent, DialogContentText, DialogActions} from '@mui/material';
+
+const GuideButton: React.FC = () => {
+
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const descriptionElementRef = React.useRef<HTMLElement>(null);
+  React.useEffect(() => {
+    if (open) {
+      const { current: descriptionElement } = descriptionElementRef;
+      if (descriptionElement !== null) {
+        descriptionElement.focus();
+      }
+    }
+  }, [open]);
+
+  return (
+    <div>
+      <Button
+        sx={{ color: 'white', borderColor: 'white', '&:hover': {backgroundColor: 'rgb(65, 77, 89)',},margin: '0px 0px 0px auto'}}
+        variant="outlined"
+        onClick={handleOpen}
+      >
+        説明
+      </Button>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        scroll="paper"
+        aria-describedby="scroll-dialog-description"
+      >
+        <DialogContent dividers={true}>
+          <DialogContentText
+            id="scroll-dialog-description"
+            ref={descriptionElementRef}
+            tabIndex={-1}
+          >
+            <h2 className="buttonBigText">使い方</h2>
+            <h4 className="buttonText">【STEP 1】</h4>
+              <p className="buttonText">
+              <Link href="https://wikiwiki.jp/taiko-fumen/%E4%BD%9C%E5%93%81/%E6%96%B0AC" underline="hover" target="_blank" rel="noopener noreferrer">
+                譜面画像
+              </Link>
+              を見ながら、リズム単位、個数、BPM、入力打数に値を入力する。<br/>
+              <div className="buttonSmallText">(ブクマに追加したり、ホーム画面に追加したりすると多分アクセスがしやすくなる)</div>
+              </p>
+
+            <h4 className="buttonText">【STEP 2】</h4>
+              <p className="buttonText">
+                随時で秒速、連打秒数、連打の理論値が確認できる。<br/><br/>
+              </p>
+
+            <hr/>
+
+            <h2 className="buttonBigText">計算式</h2>
+            <p className="buttonText1">
+              ·連打秒数 = (240*個数/リズム単位-5)/BPM<br/>
+              <div className="buttonSmallText">(連打終点では1/12拍が削られていることを考慮)<br/></div>
+              ·秒速 = 入力打数/連打秒数<br/>
+              ·理論値 = (連打秒数*60)を整数単位で切り上げた値<br/>
+              <div className="buttonSmallText">(例: 16.02打→17打、14.00打→14打)<br/><br/></div>
+            </p>
+            <p className="buttonText">
+            <Link href="https://wikiwiki.jp/taiko-fumen/%E5%8F%8E%E9%8C%B2%E6%9B%B2/%E9%80%A3%E6%89%93%E7%A7%92%E6%95%B0%E8%A1%A8" underline="hover" target="_blank" rel="noopener noreferrer">
+                参考元
+              </Link>
+              を見ると、上記の連打秒数を基準としてミリ秒単位で値を丸めて指定されていることにより、理論値や秒速がたまにズレる説や、理論値を上回って連打が入るケースが稀にあるらしいので、<b>目安程度にお願いします</b>。
+            </p>
+
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>閉じる</Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+};
+
+export default GuideButton;
